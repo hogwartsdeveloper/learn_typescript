117cb098f4f6cd4e7bb2a0c0d9128264751aef85
Zhannur <jannuraidynuly@gmail.com>
2022-01-27 23:06:19 +0500

    base type

---
+++ 1 types.ts
@@ -0,0 +1,16 @@
+const isFetching: boolean = true
+const isLoading: boolean = false
+
+const int: number = 42
+const float: number = 4.2
+const num: number = 3e10
+
+const message: string = 'Hello Typescript'
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13]
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
+
+const words: string[] = ['Hello', 'Typescript']
+
+//Tuple
+const contact: [string, number] = ['Zhannur', 87772217974]
