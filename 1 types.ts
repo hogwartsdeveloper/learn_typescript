a5f1051e52cde314fe4f89b78c8ddb122d265c7c
Zhannur <jannuraidynuly@gmail.com>
2022-01-28 10:58:13 +0500

    Any, Never, Type

--- 1 types.ts
+++ 1 types.ts
@@ -13,4 +13,38 @@ const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
 const words: string[] = ['Hello', 'Typescript']
 
 //Tuple
-const contact: [string, number] = ['Zhannur', 87772217974]
+const contact: [string, number ] = ['Zhannur', 87772217974]
+
+// Any
+let variable: any = 42
+
+variable = 'New String'
+variable = []
+
+// ====
+function sayMyName(name: string): void {
+    console.log(name)
+}
+sayMyName('Dina')
+
+// Never
+function throwError(message: string): never {
+    throw new Error(message)
+}
+
+function infinite(): never {
+    while (true) {}
+}
+
+// Type
+type Login = string
+
+const login: Login = 'admin'
+// const login2: Login = 2
+
+type ID = string | number
+const id1: ID = 1234
+const id2: ID = 'era'
+// const id3: ID = true
+
+type SomeType = string | null | undefined
